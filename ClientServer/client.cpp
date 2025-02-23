664c3d0fb7a1ae9a33ab1a54511e6af5adf6ed67
nyuta <anahit.khotsuryan@student.rau.am>
2025-02-23 20:37:02 +0400

    Added

---
+++ ClientServer/client.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <unistd.h>
+#include <cstring>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string>
+
+
+int main(){
+
+  // server address 
+  struct sockaddr_in server_address;
+
+  // create a socket
+  int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (client_socket == -1) {
+        perror("socket creation error");
+        exit(errno);
+  }
+
+  // fill the server ip and port address 
+  server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(8888);
+
+  // connect
+  int connected = connect(client_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+
+  if(connected == -1){
+    perror("connection failed");
+    exit(errno);
+  }
+
+ while (true) {
+        std::string message;
+        std::cout << "> ";
+        std::getline(std::cin, message);
+        if (message == "exit") {
+            break;
+        }
+        int sent = send(client_socket, message.c_str(), message.size(), 0);
+        if (sent == -1) {
+            perror("error sending");
+            break;
+        }
+    }
+
+  close(client_socket);
+  return 0;
+}
